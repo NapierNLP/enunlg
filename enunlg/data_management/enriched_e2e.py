d74b17d7e70678302eeaaeba0d1e7df761950125
Your Name <dave.howcroft@gmail.com>
2023-10-05 14:02:15 +0100

    create and start testing PipelineCorpusMapper callable class

--- enunlg/data_management/enriched_e2e.py
+++ enunlg/data_management/enriched_e2e.py
@@ -1,12 +1,16 @@
+from typing import Callable, Dict, Iterable, List, Optional, Union
+
+import logging
 import os
-from typing import Iterable, List, Optional, Union
 
 import box
 import xsdata.formats.dataclass.parsers as xsparsers
 
-from enunlg.formats.xml.enriched_e2e import Entries
+from enunlg.formats.xml.enriched_e2e import EnrichedE2EEntries
 import enunlg.data_management.iocorpus
+import enunlg.data_management.pipelinecorpus
 
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
 # TODO add hydra configuration for enriched e2e stuff
 ENRICHED_E2E_CONFIG = box.Box({'ENRICHED_E2E_DIR': os.path.join(os.path.dirname(__file__), '../../datasets/raw/EnrichedE2E/')})
@@ -14,7 +18,7 @@ ENRICHED_E2E_CONFIG = box.Box({'ENRICHED_E2E_DIR': os.path.join(os.path.dirname(
 E2E_SPLIT_DIRS = ('train', 'dev', 'test')
 
 
-class EnrichedE2ECorpus(enunlg.data_management.iocorpus.IOCorpus):
+class EnrichedE2ECorpusRaw(enunlg.data_management.iocorpus.IOCorpus):
     def __init__(self, seq: Optional[Iterable] = None, filename_or_list: Optional[Union[str, List[str]]] = None):
         if seq is None:
             seq = []
@@ -23,19 +27,50 @@ class EnrichedE2ECorpus(enunlg.data_management.iocorpus.IOCorpus):
         if filename_or_list is not None:
             if isinstance(filename_or_list, list):
                 for filename in filename_or_list:
-                    print(filename)
+                    logging.info(filename)
                     self.load_file(filename)
             elif isinstance(filename_or_list, str):
                 self.load_file(filename_or_list)
             else:
                 raise TypeError(f"Expected filename_or_list to be None, str, or list, not {type(filename_or_list)}")
+            self.extend(self.entries)
 
     def load_file(self, filename):
-        entries_object = xsparsers.XmlParser().parse(filename, Entries)
-        self.entries.extend(entries_object.entry)
+        entries_object = xsparsers.XmlParser().parse(filename, EnrichedE2EEntries)
+        self.entries.extend(entries_object.entries)
 
 
-def load_enriched_e2e(splits: Optional[Iterable[str]] = None, enriched_e2e_config: Optional[box.Box] = None) -> EnrichedE2ECorpus:
+class PipelineCorpusMapper(object):
+    def __init__(self, input_format, output_format, annotation_layer_mappings: Dict[str, Callable]):
+        """
+        Create a function which will map from `input_format` to `output_format` using `annotation_layer_mappings`.
+        """
+        self.input_format = input_format
+        self.output_format = output_format
+        self.annotation_layer_mappings = annotation_layer_mappings
+        logging.info('successful init')
+
+    def __call__(self, input_corpus):
+        logging.info('successful call')
+        if isinstance(input_corpus, self.input_format):
+            logging.info('passed the format check')
+            output_seq = []
+            for x in input_corpus:
+                output = []
+                for layer in self.annotation_layer_mappings:
+                    logging.info(f"processing {layer}")
+                    output.append(self.annotation_layer_mappings[layer](x))
+                max_length = max([len(x) for x in output])
+                output = [x * max_length if len(x) == 1 else x for x in output]
+                assert all([len(x) == max_length for x in output]), f"expected all layers to have the same number of items, but received: {[len(x) for x in output]}"
+                output_seq.append(output)
+                logging.info(len(output_seq))
+            return self.output_format(output_seq)
+        else:
+            raise TypeError(f"Cannot run {self.__class__} on {type(input_corpus)}")
+
+
+def load_enriched_e2e(splits: Optional[Iterable[str]] = None, enriched_e2e_config: Optional[box.Box] = None) -> EnrichedE2ECorpusRaw:
     """
 
     :param splits: which splits to load
@@ -54,11 +89,49 @@ def load_enriched_e2e(splits: Optional[Iterable[str]] = None, enriched_e2e_confi
         raise ValueError(f"`splits` can only contain a subset of {default_splits}. Found {splits}.")
     fns = []
     for split in splits:
-        print(split)
+        logging.info(split)
         fns.extend([os.path.join(data_directory, split, fn) for fn in os.listdir(os.path.join(data_directory, split))])
 
-    corpus = EnrichedE2ECorpus(filename_or_list=fns)
+    corpus = EnrichedE2ECorpusRaw(filename_or_list=fns)
     corpus.metadata = {'name': corpus_name,
                        'splits': splits,
                        'directory': data_directory}
+    logging.info(len(corpus))
+    #lambda entry: [x.sentence.content for x in entry.targets.structuring]
+
+    def extract_ordered_input(entry):
+        targets = []
+        for target in entry.targets:
+            selected_inputs = []
+            for sentence in target.structuring.sentences:
+                selected_inputs.extend(sentence.content)
+            targets.append(selected_inputs)
+        return targets
+
+    def extract_sentence_segmented_input(entry):
+        targets = []
+        for target in entry.targets:
+            selected_inputs = []
+            for sentence in target.structuring.sentences:
+                selected_inputs.append(sentence.content)
+            targets.append(selected_inputs)
+        return targets
+
+    def extract_lexicalization(entry):
+        return [target.lexicalization for target in entry.targets]
+
+    def extract_raw_output(entry):
+        return [target.text for target in entry.targets]
+
+    enriched_e2e_factory = PipelineCorpusMapper(EnrichedE2ECorpusRaw, enunlg.data_management.iocorpus.IOCorpus,
+                                                {'raw-input': lambda entry: [entry.source.inputs],
+                                                 'selected-input': lambda entry: [set(x) for x in extract_ordered_input(entry)],
+                                                 'ordered-input': extract_ordered_input,
+                                                 'sentence-segmented-input': extract_sentence_segmented_input,
+                                                 'lexicalisation': extract_lexicalization,
+                                                 'referring-expressions': lambda x: [None],
+                                                 'raw-output': extract_raw_output})
+    logging.info(len(corpus))
+    corpus = enriched_e2e_factory(corpus)
+    logging.info(len(corpus))
     return corpus
