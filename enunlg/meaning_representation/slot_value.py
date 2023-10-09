8259d9b934ef60934a5efdc9bb0a926a39f5d9a6
Your Name <dave.howcroft@gmail.com>
2023-10-09 18:02:48 +0100

    add MultivaluedSlotValueMR for when we want to do MultivaluedDAs with these

--- enunlg/meaning_representation/slot_value.py
+++ enunlg/meaning_representation/slot_value.py
@@ -11,3 +11,15 @@ class SlotValueMR(box.Box):
 
     def __str__(self):
         return self.__repr__()
+
+
+class MultivaluedSlotValueMR(box.Box):
+    def __init__(self, *args, **kwargs):
+        super(SlotValueMR, self).__init__(*args, default_box=True, default_box_attr=list, **kwargs)
+
+    def __repr__(self):
+        slot_value_pairs = ", ".join([f"{key}='{self[key]}'" for key in self.keys()])
+        return f"{self.__class__.__name__}({slot_value_pairs})"
+
+    def __str__(self):
+        return self.__repr__()
