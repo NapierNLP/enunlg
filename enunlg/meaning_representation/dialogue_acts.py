69b431ac79980401eb3c610ba520bb45ee797899
Dave Howcroft <dave.howcroft@gmail.com>
2024-09-23 17:15:59 +0100

    update MR-related classes

--- enunlg/meaning_representation/dialogue_acts.py
+++ enunlg/meaning_representation/dialogue_acts.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
 from typing import Iterable, Tuple
 
+import logging
+
 import box
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -27,10 +31,19 @@ class MultivaluedDA(DialogueAct):
         """
         super(MultivaluedDA, self).__init__(act_type, slot_values)
 
+    def __len__(self):
+        return sum([len(self.slot_values[slot]) for slot in self.slot_values])
+
+    def __hash__(self):
+        return hash((self.act_type, box.Box(self.slot_values, frozen_box=True)))
+
     @staticmethod
     def from_slot_value_list(act_type, slot_values: Iterable[Tuple[str, str]]) -> "MultivaluedDA":
         """Create a MultivalueDA when we have an iterable of slot-value pairs rather than a ready-to-go Box."""
         slot_value_box = box.Box(default_box=True, default_box_attr=list)
         for slot, value in slot_values:
-            slot_value_box[slot].append(value)
+            if slot in slot_value_box:
+                slot_value_box[slot].append(value)
+            else:
+                slot_value_box[slot] = [value]
         return MultivaluedDA(act_type, slot_value_box)
