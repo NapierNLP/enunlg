63de803574b631f87ed13d52d0e3e91df06f2ba4
Dave Howcroft <dave.howcroft@gmail.com>
2024-09-23 17:19:51 +0100

    cleanup fetch_dataset() and add process_datset()

--- scripts/fetch_dataset.bash
+++ scripts/fetch_dataset.bash
@@ -24,22 +24,59 @@ function fetch_dataset {
         exit 1
       fi ;;
     cued)
-      git clone git@github.com:shawnwun/RNNLG.git ;;
+      if [ ! -d RNNLG ]
+      then
+        git clone https://github.com/shawnwun/RNNLG.git
+      fi ;;
     e2e)
       wget https://github.com/tuetschek/e2e-dataset/releases/download/v1.0.0/e2e-dataset.zip
       unzip e2e-dataset.zip ;;
     e2e-cleaned)
       git clone git@github.com:tuetschek/e2e-cleaning.git ;;
     e2e-enriched)
-      git clone git@github.com:ThiagoCF05/EnrichedE2E.git ;;
+      if [ ! -d EnrichedE2E ]
+      then
+        git clone https://github.com/ThiagoCF05/EnrichedE2E.git
+      fi;;
     neural-methodius)
       git clone git@github.com:aleksadre/methodiusNeuralINLG2021 ;;
     webnlg2023)
       git clone git@github.com:WebNLG/2023-Challenge.git ;;
     webnlg-enriched)
-      git clone git@github.com:ThiagoCF05/webnlg.git ;;
+      if [ ! -d webnlg ]
+      then
+        git clone https://github.com/ThiagoCF05/webnlg.git
+      fi;;
   esac
 }
+
+
+function process_dataset {
+  case $1 in
+    cued)
+      mkdir -p datasets/glove-vectors
+      tail -n10164 datasets/raw/RNNLG/vec/vectors-80.txt > datasets/glove-vectors/rnnlg.vectors-80.txt ;;
+    e2e-enriched)
+      cp -rf datasets/raw/EnrichedE2E datasets/processed/.
+      # Fix errors  in corpus annotation
+      # TODO check usage of __s__
+      sed -i -e 's/__CUSTOMER___ RATING/__CUSTOMER_RATING__/g' -e 's/__CUSTOMER___ __RATING\([a-z\-]*\)__/__CUSTOMER_RATING__ \1/g' -e 's/ CUSTOMER_RATING //g' -e 's/\([ .]\)__ /\1/g' -e 's/__ @ FAMILYFRIENDLY /__ @ __FAMILYFRIENDLY__ /g' -e 's/<text>The Wrestlers is \(a decent\|an average\) \(family-\|child \)friendly \(place\|venue\).</<text>The Wrestlers is \1 @ \2friendly \3.</g' -e 's/Not family friendly Alimentum/Not family friendly @ Alimentum/g' -e 's/high-priced kids-friendly/high-priced @ kids-friendly/g' -e "s/Its a /It's a /g" -e "s/ __NAME__s / __NAME__ 's /g" -e "s/ __s__ / /g" -e 's/verage family friendly/verage @ family friendly/g' -e 's/ it high/ it @ high/g' -e 's/__..but/but/g' -e 's/\.\.\.but/, but/g' -e 's/for children it/for children @ it/g' -e 's/__CUSTOMER_RATING__and/__CUSTOMER_RATING__ and/g' -e 's/5and/5 and/g' -e "s/ ' \(s\|re\) / '\1 /g" -e 's/ __FAMILYFRIENDLY__t family - friendly / __FAMILYFRIENDLY__ /g' -e 's/ \(__PRICERANGE__ly\|__PRICERANGEly__\) / __PRICERANGE__ /g' datasets/processed/EnrichedE2E/*/*.xml
+      head -n2405 datasets/processed/EnrichedE2E/dev/3attributes.xml > tmp.3atts
+      echo '        <sentence>' >> tmp.3atts
+      echo '          <input attribute="name" tag="__NAME__" value="The Phoenix"/>' >> tmp.3atts
+      echo '          <input attribute="area" tag="__AREA__" value="riverside"/>' >> tmp.3atts
+      echo '          <input attribute="customer rating" tag="__CUSTOMER_RATING__" value="5 out of 5"/>' >> tmp.3atts
+      echo '        </sentence>' >> tmp.3atts
+      tail -n2923 datasets/processed/EnrichedE2E/dev/3attributes.xml >> tmp.3atts
+      mv tmp.3atts datasets/processed/EnrichedE2E/dev/3attributes.xml;;
+    webnlg-enriched)
+      cp -rf datasets/raw/webnlg datasets/processed/.
+      # Fix errors in corpus annotation
+      sed -i -e 's/"BRIDGE-"/"BRIDGE-2"/g' -e 's/ BRIDGE- / BRIDGE-2 /g' datasets/processed/webnlg/data/v1.6/en/dev/3triples/Building.xml
+#      sed -i -e 's/\(AGENT\|BRIDGE\|PATIENT\)-\([0-9]\)/__\1_\2__/g' datasets/processed/webnlg/data/v1.6/en/*/*/*.xml;;
+  esac
+}
+
 
 function usage {
     echo "Usage:    ./fetch_dataset.bash DATASET"
@@ -60,12 +97,17 @@ DATASET=$1
 
 if [ -v SUPPORTED_DATASETS["${DATASET}"] ]
 then
+  INITIAL_WD=$(pwd)
   echo "Fetching ${DATASET}..."
   mkdir -p datasets/raw
   cd datasets/raw || exit
   pwd
   fetch_dataset "${DATASET}"
-  cd -
+  cd ${INITIAL_WD} || exit
+  pwd
+  echo "Postprocessing ${DATASET}"
+  mkdir -p datasets/processed
+  process_dataset "${DATASET}"
 else
     echo ""
     echo "ERROR: No rule for fetching ${DATASET}"
